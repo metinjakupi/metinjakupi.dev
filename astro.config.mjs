f3c6584b829cbc16779ec66b13df7cef85f8b3e1
mjakupi <jakupi@eagle.mk>
2022-08-21 18:51:19 +0200

    feat: partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import vue from "@astrojs/vue";
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+    integrations: [vue(), tailwind(), partytown({
+        // Adds dataLayer.push as a forwarding-event.
+        config: {
+            forward: ["dataLayer.push"],
+        },
+    }),]
 });
