99de90fe07941b09243a99fe7ef6b7b5ee794047
mjakupi <jakupi@eagle.mk>
2023-10-02 16:45:28 +0200

    feat: new design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,174 +1,94 @@
+<style>
+.box__bg {
+  position: relative;
+  z-index: 0;
+
+  overflow: hidden;
+  padding: 12px;
+}
+
+.box__bg::before {
+  content: "";
+  position: absolute;
+  z-index: -2;
+  left: -50%;
+  top: -50%;
+  width: 200%;
+  height: 200%;
+  background-color: #000;
+  background-repeat: no-repeat;
+  background-size: 100%100%, 50%50%;
+  background-position: 0 0, 100% 0, 100% 100%, 0 100%;
+  background-image: linear-gradient(#f3f4f6, #2e1065);
+  animation: bgRotate 4s linear infinite;
+}
+
+.box__bg::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  left: 1px;
+  top: 1px;
+  width: calc(100% - 2px);
+  height: calc(100% - 2px);
+  background: linear-gradient(#101010, #101010);
+  border-radius: 12px;
+}
+
+@keyframes bgRotate {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+
+@media (max-width: 768px) {
+  .box{
+    grid-column: span 3!important;
+  }
+}
+
+</style>
 <template>
-  <main class="max-w-4xl p-3 mx-auto my-auto bg-gray-100 ">
-    <header class="text-left w-full mb-3 align-top border-b-8">
-        <h1 class="text-3xl mb-2 font-bold text-gray-700">
-          Metin Jakupi
-        </h1>
-        <h2 class="font-semibold text-gray-700 mb-2">
-         Senior Frontend Developer
-        </h2>
-    </header>
+  <main class="max-w-7xl p-3 mx-auto my-auto">
 
-    <div class="col-gap-16">
-      <section class="pb-4 mt-4 first:mt-0">
-        <div class="pb-2 mb-2 border-b-2">
-          <ul class="list-inside pr-7">
+    <div :class="['grid', 'grid-cols-3', 'gap-6', 'p-4']">
+      <div
+          :key="i" v-for="(item, i) in gridItems"
+          :href="item.link"
+          :class="[
+        item.classes,
+        'box',
+        'fade-in-animation',
+        'rounded-xl',
+        'shadow-lg',
+        'p-6',
+        'font-sans',
+        'transform',
+        'hover:scale-105',
+        'transition-transform',
+      ]"
+          :style="{ 'grid-column': 'span ' + item.colSpan }"
+      >
+        <h3 class="text-2xl font-bold mb-4">{{ item.title }}</h3>
+        <img v-if="item.icon && item.isLogo" :src="item.icon" class=" mt-4" alt="">
+        <p v-if="item.content" v-html="item.content" class="text-lg mb-4"></p>
+        <img v-if="item.icon && !item.isLogo" :src="item.icon" width="50" alt="">
+        <div v-if="item.skills">
+          <span v-for="skill in item.skills"
+                class="inline-flex items-center rounded-md bg-gray-50 my-2 mx-1.5 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+            {{ skill }}
+          </span>
+        </div>
+      </div>
 
-            <li class="mt-1 leading-normal transition duration-100 ease-in text-gray-500 text-md hover:text-gray-700">
-              <a href="https://github.com/metinjakupi" class="group">
-                      <span class="mr-5 text-lg font-semibold text-gray-700">
-                        Github:
-                      </span>
-                metinjakupi
-                <span
-                    class="inline-block font-normal text-black transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700 print:">
-                        ↗
-                      </span>
-              </a>
-            </li>
-
-            <li class="mt-1 leading-normal transition duration-100 ease-in text-gray-500 text-md hover:text-gray-700">
-              <a href="mailto:mjakupi@outlook.com" class="group">
-                      <span class="mr-8 text-lg font-semibold text-gray-700">
-                        Email:
-                      </span>
-                mjakupi@outlook.com
-                <span
-                    class="inline-block font-normal transition duration-100 ease-in text-gray-500 print:text-black group-hover:text-gray-700">
-                        ↗
-                      </span>
-              </a>
-            </li>
-            <li class="mt-1 leading-normal transition duration-100 ease-in text-gray-500 text-md hover:text-gray-700">
-                      <span class="mr-2 text-lg font-semibold text-gray-700">
-                        Location:
-                      </span>Ohrid, North Macedonia
-            </li>
-          </ul>
-        </div>
-      </section>
-      <section class="pb-2 pb-4 mt-4 border-b-4 first:mt-0">
-        <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
-          SUMMARY
-        </h2>
-        <div class="mb-2">
-          <p class="mt-2 leading-normal text-gray-700 text-md">
-            Highly motivated Senior Frontend Developer with a strong track record of bringing projects to fruition.
-            Excellent
-            problem-solving and analytical skills, with experience architecting, implementing, and testing distributed
-            systems
-            and highly scalable web applications. Proven track record of understanding user needs and translating them
-            into
-            actionable solutions. A self-starter with the ability to work independently and in teams to accomplish major
-            projects
-          </p>
-        </div>
-      </section>
-      <section class="pb-2 pb-4 mt-4 border-b-4 first:mt-0">
-        <div>
-          <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
-            EXPERIENCE
-          </h2>
-          <section class="mb-2">
-            <header v-for="(j,k) in jobs" :key="`job-${k}`" class="mb-5">
-
-              <h3 class="text-lg font-semibold text-gray-700">
-                {{ j.title }}
-              </h3>
-              <p class="leading-normal text-md text-gray-600">
-                {{ j.dates }} {{ j.company }}
-              </p>
-              <p>
-                {{ j.description }}
-              </p>
-            </header>
-          </section>
-        </div>
-      </section>
-      <section class="pb-2 pb-4 mt-4 border-b-4 first:mt-0">
-        <div>
-          <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
-            EDUCATION
-          </h2>
-          <section class="pb-4 mt-4 mb-4">
-            <header>
-              <h3 class="flex-grow text-lg font-semibold text-gray-700 leading-snugish">
-                University for Information Science and Technology "St. Paul the Apostle"
-              </h3>
-              <p class="leading-normal text-md text-gray-500">
-                2014 – 2018
-              </p>
-            </header>
-          </section>
-        </div>
-      </section>
-      <section class="pb-4 mt-4 first:mt-0">
-        <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
-          SKILLS
-        </h2>
-        <div class="mb-2">
-          <header>
-            <h3 class="text-lg font-semibold text-gray-700 leading-snugish">
-              Full Stack Web Development
-            </h3>
-          </header>
-
-          <div class="my-1 last:pb-1">
-            <ul class="flex flex-wrap text-sm2 leading-relaxed -mr-1.6 -mb-1">
-              <li v-for="(skill,k) in skills" :key="`skill-${k}`"
-                  class="px-2.5 mr-2 mb-1 text-white leading-relaxed  bg-indigo-400 print:bg-white print:border-inset print:text-black">
-                {{ skill }}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="mb-2">
-          <header>
-            <h3 class="font-semibold text-gray-700 text-m leading-snugish">
-              DevOps
-            </h3>
-          </header>
-          <div class="my-1 last:pb-1">
-            <ul class="flex flex-wrap text-sm2 leading-relaxed -mr-1.6 -mb-1">
-              <li v-for="(dev,k) in devOps" :key="`devOps-${k}`"
-                  class="px-2 mr-2 mb-1 text-black leading-relaxed bg-emerald-300 print:bg-white print:border-inset ">
-                {{ dev }}
-              </li>
-
-            </ul>
-          </div>
-        </div>
-      </section>
     </div>
   </main>
 </template>
 
 <script lang="ts" setup>
 
-const jobs = [
-  {
-    title: "Freelance",
-    company: "",
-    location: "",
-    dates: "January 2015 – April 2017",
-    description: "Web & Mobile development",
-  },
-  {
-    title: "Senior Frontend Developer",
-    company: "| Eagle It Solutions",
-    location: "Struga, North Macedonia",
-    dates: "August 2017 – Present",
-    description: "Proactively cultivated client relationships designed full stack web experiences for clients actively engaged with clients about specifications and designs crafted literate, comprehensive source code provided commentary to improve maintainability fostered ongoing services to clients as requested or needed",
-  },
-  {
-    title: "Full Stack Developer",
-    company: "| Zappter",
-    location: "Zürich, Switzerland",
-    dates: "January – April 2019",
-    description: "Building plugins for Zappter platform. Using PHP and JQuery",
-  },
+import {ref} from "vue";
 
-]
 const skills = [
   'HTML5',
   'CSS3',
@@ -177,17 +97,13 @@ const skills = [
   'Javascript',
   'Typescript',
   'Node.js',
-  'Vue',
+  'Vue.js',
   'Wordpress',
   'Quasar',
   'Ionic',
-  'Angular',
   'Nuxt.js',
   'React',
-  'Gatsby.js',
-]
-
-const devOps = [
+  'Next.js',
   'AWS',
   'Terraform',
   'Docker',
@@ -197,6 +113,59 @@ const devOps = [
   'Serverless Hosting',
   'CircleCI',
 ]
+const gridItems = ref([
+  {
+    title: 'Introduction',
+    content: 'Hello, I am <b class="text-lg"> Metin Jakupi</b>. Dedicated Developer with a proven history of successful projects. Skilled in problem-solving and system architecture, adept at creating scalable web applications. Expert in understanding user requirements and delivering effective solutions. Capable of working autonomously and collaboratively.',
+    classes: 'border text-white border-zinc-400',
+    colSpan: 2,
+    link: '#',
+  },
+  {
+    title: '',
+    link: 'https://github.com/yourusername',
+    icon: 'github-mark.png',
+    isLogo: false,
+    classes: 'flex justify-center items-center flex-col border border-zinc-400 text-white',
+    colSpan: 1
+  },
+  {
+    title: '',
+    link: 'https://twitter.com/mjakupiiii',
+    icon: 'logo-white.png',
+    isLogo: false,
+    classes: 'flex justify-center items-center flex-col border border-zinc-400 text-white',
+    colSpan: 1
+  },
+  {
+    title: 'My Skills',
+    content: 'Here is my tech stack',
+    link: '#',
+    classes: 'border text-white border-zinc-400',
+    skills: skills,
+    colSpan: 1
+  },
+  {
+    title: 'Work Experience',
+    isLogo: true,
+    link: 'https://eagle.mk/',
+    icon: 'logo.png',
+    content: `
+    <strong>Senior Frontend Developer</strong><br>
+    August 2017 – Present | Eagle IT Solutions`,
+    classes: 'box__bg text-white',
+    colSpan: 1
+  },
+  {
+    title: 'Contact Me',
+    content: 'You can reach me at mjakupi@outlook.com or through my social profiles.',
+    classes: 'border text-white border-zinc-400',
+    colSpan: 2,
+    link: '#',
+  }
+])
+
+
 </script>
 
 <style scoped>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,29 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				border: 'border 4s ease infinite',
+				flip: "flip 6s infinite steps(2, end)",
+				rotate: "rotate 3s linear infinite both",
+			},
+			keyframes: {
+				border: {
+					'0%, 100%': { backgroundPosition: '0% 50%' },
+					'50%': { backgroundPosition: '100% 50%' },
+				},
+				flip: {
+					to: {
+						transform: "rotate(360deg)",
+					},
+				},
+				rotate: {
+					to: {
+						transform: "rotate(90deg)",
+					},
+				},
+			},
+		},
 	},
 	plugins: [],
 }
